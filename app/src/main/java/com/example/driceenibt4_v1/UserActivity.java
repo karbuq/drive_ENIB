6799f385c3a2cb139317c0f5ddd037b71dc725b9
Kleden <kleden.lechanjour@gmail.com>
2021-05-29 23:20:22 +0200

    interface avec dataBase mais ça pas ??

--- app/src/main/java/com/example/driceenibt4_v1/UserActivity.java
+++ app/src/main/java/com/example/driceenibt4_v1/UserActivity.java
@@ -1,15 +1,71 @@
 package com.example.driceenibt4_v1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class UserActivity extends AppCompatActivity {
+
+    //variable du layout
+    private EditText Prenom,Nom,NumTel,Age,Email;
+    private RadioButton mRadioButton;
+    private Button mButton;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_activity);
+
+        //Lien entre xml et le java
+        Prenom=findViewById(R.id.username);
+        Nom=findViewById(R.id.username2);
+        NumTel=findViewById(R.id.phonenumber);
+        Age=findViewById(R.id.userage);
+        mRadioButton=findViewById(R.id.accepterCondition);
+        mButton=findViewById(R.id.ComfirmUser);
+        Email=findViewById(R.id.email);
+
+        //Enregistrer les donner dans Firebase
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //recuperer les valeurs
+                String prenom=Prenom.getText().toString();
+                String nom=Nom.getText().toString();
+                String age=Age.getText().toString();
+                String numerotel=NumTel.getText().toString();
+                String email=Prenom.getText().toString();
+
+                //rajouter les conditions
+
+                UserDataClass dataClass=new UserDataClass(prenom,nom,age,numerotel,email);
+
+                rootNode=FirebaseDatabase.getInstance();
+                reference=rootNode.getReference("Users");
+                reference.child(prenom).setValue(dataClass);
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("massage");
+
+                myRef.setValue("Hello, World!");
+
+                Toast.makeText(UserActivity.this,"Appuis bouton enregistré",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/driceenibt4_v1/UserDataClass.java
@@ -0,0 +1,58 @@
+package com.example.driceenibt4_v1;
+
+public class UserDataClass {
+
+    public String prenom,nom,numTel,age,email;
+
+    public UserDataClass(){
+
+    }
+
+    public UserDataClass(String prenom, String nom, String numTel, String age,String email) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.numTel = numTel;
+        this.age = age;
+        this.email=email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNumTel() {
+        return numTel;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
