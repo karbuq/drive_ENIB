3e8379223c0d3237ca7a93bf9e46a685b45ec886
Kleden <kleden.lechanjour@gmail.com>
2021-05-31 14:23:55 +0200

    Cnnexion et base de donner optimiser, modification profil 1.1 avec lecture base de donné

--- app/src/main/java/com/example/driceenibt4_v1/ProfilActivity.java
+++ app/src/main/java/com/example/driceenibt4_v1/ProfilActivity.java
@@ -1,20 +1,58 @@
 package com.example.driceenibt4_v1;
 
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
 public class ProfilActivity extends AppCompatActivity {
 
+    private static final String TAG = "ProfilActivity";
+
+    private TextView retourTv;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        retourTv=findViewById(R.id.reto);
+
+        retourTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilActivity.this, MenuActivty.class);
+                startActivity(intent);
+                finish();
+
+            }
+    });
+
+
+
     }
+
+
 }
+
 
